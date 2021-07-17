c62912a65948c85fac201166fe68bb63ac4ada8f
mcraet <mcraeteisha@gmail.com>
2021-07-16 23:33:23 -0400

    updating database name

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost/budget", {
+mongoose.connect("mongodb://localhost/BudgetDB", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
